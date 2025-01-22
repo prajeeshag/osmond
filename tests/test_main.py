ca003dc952488bab28d24ec44c3d113c46f12768
Prajeesh Ag <prajeeshag@gmail.com>
2025-01-22 09:46:18 +0300

    ✏️ typo: change test filename to glbl

--- tests/test_main.py
+++ tests/test_main.py
@@ -2,7 +2,7 @@ import glob
 
 from osmond import create_domain, subset_forcings
 
-create_domain("GEBCO_2024_sub_ice_topo.nc", 20.0, 60.0, 10.0, 35.0, "output/test")
+create_domain("GEBCO_2024_sub_ice_topo.nc", 20.0, 60.0, 10.0, 35.0, "output/glbl")
 
 atmos_files = glob.glob("test_data/atmosphere/2025012012/*.nc")
 subset_forcings(atmos_files, 20.0, 60.0, 10.0, 35.0, "output/atmos/")
