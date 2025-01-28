72d7f898678a1f7b8fc7098882931c413064015a
Prajeesh Ag <prajeeshag@gmail.com>
2025-01-28 10:24:09 +0300

    ➕ dep-add: Added matplotlib

--- tests/test_domain.py
+++ tests/test_domain.py
@@ -7,6 +7,8 @@ from osmond import (
     process_wave_files,
 )
 
-box = [-20.0, -10.0, 10.0, 30.0]
+# box = [-20.0, -10.0, 10.0, 30.0]
+# box = [17.39, 19.289, 32.47, 34.27]
+box = [50.86, 53.81, 25.45, 27.25]
 
 create_domain("GEBCO_2024_sub_ice_topo.nc", box[0], box[1], box[2], box[3], "test")
